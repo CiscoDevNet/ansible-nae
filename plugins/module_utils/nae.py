922240fa2fe07747ca94736d22d5bde5f5638ef7
cindy zhao <xinyuezhao96@gmail.com>
2020-11-16 20:40:29 -0800

    Add parameter to set req set status as active/inactive

--- plugins/module_utils/nae.py
+++ plugins/module_utils/nae.py
@@ -1116,7 +1116,7 @@ class NAEModule(object):
         d = json.loads(self.params.get('form'))
         assurance_groups_lists = []
         if self.params.get('association_to_ag'):
-            assurance_groups_lists.append(dict(active=True, fabric_uuid=ag.get('uuid')))
+            assurance_groups_lists.append(dict(active=self.params.get('active'), fabric_uuid=ag.get('uuid')))
         d['assurance_groups'] = assurance_groups_lists
         self.params['form'] = json.dumps(d)
         if '5.1' in self.version or '5.0' in self.version:
